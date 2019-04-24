8501900557e0621a14b44107ce1e39b55ef135b4
betzsoldn <nick.betzsold@hotmail.com>
2019-04-24 08:32:59 -0700

    intial commit with sample app skeleton

---
+++ app/global.R
@@ -0,0 +1,9 @@
+# This contains the necessary libraries and global variables/functions
+#---------------------------------------------------------------------
+
+#----- Needed Libraries -----
+library(shiny)
+
+#----- Global Variables -----
+
+#----- Global Functions -----
